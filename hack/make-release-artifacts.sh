cb03b07f4781953e544e20fed0594e1d238918d1
kaonmir <thswpvm@cau.ac.kr>
2022-02-22 20:09:33 +0900

    remove istio

--- hack/make-release-artifacts.sh
+++ hack/make-release-artifacts.sh
@@ -96,10 +96,6 @@ main() {
     k8s_manifests_file="${OUT_DIR}/kubernetes-manifests.yaml"
     mk_kubernetes_manifests > "${k8s_manifests_file}"
     log "Written ${k8s_manifests_file}"
-
-    istio_manifests_file="${OUT_DIR}/istio-manifests.yaml"
-    mk_istio_manifests > "${istio_manifests_file}"
-    log "Written ${istio_manifests_file}"
 }
 
 main
